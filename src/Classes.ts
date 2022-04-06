ae85f125fe975f0d608e97e217a3f731d11ecd9c
sergey <zhdanushkaa@yandex.ru>
2022-04-06 13:26:45 +0500

    Задание по классам

--- src/Classes.ts
+++ src/Classes.ts
@@ -1,3 +1,5 @@
+import { NamedTupleMember } from 'typescript';
+
 /*
 Реализуйте класс Counter:
 - Значение счётчика хранится в поле counter: number. Значение поля по умолчанию: 0
@@ -5,4 +7,20 @@
 - Класс должен иметь метод increment, который увеличивает counter на 1 и возвращает результат
 - Класс должен иметь метод decrement, который уменьшает counter на 1 и возвращает результат
  */
-export class Counter {}
+export class Counter {
+    counter: number = 0;
+
+    constructor(start?: number) {
+        if (start !== undefined) this.counter = start;
+    }
+
+    increment(): number {
+        this.counter++;
+        return this.counter;
+    }
+
+    decrement(): number {
+        this.counter--;
+        return this.counter;
+    }
+}
